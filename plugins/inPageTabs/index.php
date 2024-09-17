507364df00e59f9fa9fb8b23c9774d33388bc4d3
Dan Gilmore <2inchesofwater@users.noreply.github.com>
2024-09-17 15:10:54 +1000

    Update index.php
    Changed the name of the plugin, to reflect Kirby 4 best practices.

--- plugins/inPageTabs/index.php
+++ plugins/inPageTabs/index.php
@@ -1,11 +1,11 @@
 <?php
+// Adapted from https://getkirby.com/docs/cookbook/panel/first-panel-field
 
-Kirby::plugin('sodaville/inPageTabs', [
+Kirby::plugin('sodaville/kirby4-inpagetabs', [
     'fields' => [
-        'inPageTabs' => [
+        'inpagetabs' => [
           // here we could define the backend logic for our field if needed
         ]
     ]
 ]);
-
 ?>
