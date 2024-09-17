67767f1327b1361ded87efd6e617d8d23637421c
Dan Gilmore <2inchesofwater@users.noreply.github.com>
2024-09-17 15:18:54 +1000

    Update index.js
    Changed the name of the plugin; added a comment with the origin of the code

--- plugins/inPageTabs/src/index.js
+++ plugins/inPageTabs/src/index.js
@@ -1,6 +1,9 @@
-import inPageTabs from "./components/fields/inPageTabsCore.vue";
-panel.plugin('sodaville/inPageTabs', {
+import inPageTabs from "@/components/fields/inpagetabs-core.vue";
+
+/* Adapted from https://getkirby.com/docs/cookbook/panel/first-panel-field */
+
+panel.plugin('sodaville/kirby4-inpagetabs', {
   fields: {
-    inpagetabs: inPageTabsCore   
+    inpagetabs: inPageTabs
   }
 });
